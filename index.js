91e25733b46979f72e1fcaa920496919e723ebad
Yousef. <kingyousef922@gmail.com>
2023-10-11 13:44:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function   findMatching(drivers, name){
+    const test = 
+    drivers.filter(function(driver){return driver.toLowerCase() === name.toLowerCase()}) 
+    return test}
+
+    function fuzzyMatch(drivers, attribute) {
+        return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+      }
+      
+      function matchName(drivers, argument) {
+        return drivers.filter(function (driver) { return driver.name === argument })
+      }
+
